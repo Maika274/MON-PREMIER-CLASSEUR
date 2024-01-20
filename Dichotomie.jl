cdedebb7e780c6425ab711133dd93c2c132c6564
Maika274 <153763945+Maika274@users.noreply.github.com>
2024-01-20 17:08:43 -0400

    Update Dichotomie.jl

--- Dichotomie.jl
+++ Dichotomie.jl
@@ -1,7 +1,7 @@
 function f(x)
-  print("Saisir a")
+  print("Saisir a \n")
   a=parse(Float64,redline())
-  print("Saisir b")
+  print("Saisir b \n")
   b=parse(Float64,redline())
   x=x.^2 .+ 2*x -3
   return f(x)
@@ -10,6 +10,7 @@ end
 
 function Dichotomie(f(x),x0,n)
   x0= (a.+b)./2
+  n=1
   while f(x0)!=0
     if f(x0)<0
       a=x0
@@ -19,6 +20,8 @@ function Dichotomie(f(x),x0,n)
       b=x0
     end
     x0=(a.+b)./2
+    n=n+1
   end
 return x0
+print("Il a fallut $n itérations pour obtenir $x0 \n")
 end
