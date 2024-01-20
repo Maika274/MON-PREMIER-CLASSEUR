981023b64d90ab70e64506909a69cb39f48bbaef
Maika274 <153763945+Maika274@users.noreply.github.com>
2024-01-20 15:38:47 -0400

    Update Dichotomie.py

--- Dichotomie.py
+++ Dichotomie.py
@@ -1,18 +1,17 @@
-def f(x)
+def f(x):
   x=x.^2 .+ 2*x -3
   return f(x)
-end
 
 
-function Dichotomie(f(x),x0,n)
+def Dichotomie(f(x),x0,n):
   a=float(input("Saisir a"))
   b=float(input("Saisir b"))
   x0= (a.+b)./2
-  while f(x0)!=0
-    if f(x0)<0
+  while f(x0)!=0:
+    if f(x0)<0:
       a=x0
       b=b
-    else
+    else:
       a=a
       b=x0
     x0=(a.+b)./2
