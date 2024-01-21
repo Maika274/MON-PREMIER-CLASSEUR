a016ecc126ffb5b2147c0d2fc1feaba9c4d4f549
Maika274 <153763945+Maika274@users.noreply.github.com>
2024-01-20 20:24:27 -0400

    Update Exercice 1 (Dichotomie).py

--- Dichotomie.py
+++ Dichotomie.py
@@ -16,3 +16,39 @@ def Dichotomie(f(x),x0,n):
       b=x0
     x0=(a.+b)./2
 return x0
+
+
+function Norme(X) # Création d'une fonction Norme afin de calculer l'erreur
+    N=sqrt(X'*X);
+    return   N[1];
+end
+
+function Preci()  
+  print("Saisir un nombre initial i : \n")
+  i=parse(Float64,redline())
+  E=Norme(i-x0);
+  n=1;
+  while (E>10^(-12))
+    P=Dichotomie(f(x),x0,n)
+    E=Norme(P[:,n+1]-x0);
+  n=n+1;
+end
+print("Pour $i, il faut $(n-1) itérations pour obtenir une erreur inférieure à 10^(-12).\n");
+
+  #Question 3. b)
+
+  function Temps
+     T=0;
+    for n=1:n
+        T=T+@elapsed(Preci());
+    end
+    print("Il faut $T secondes afin d'obtenir une telle précision. \n")
+end
+
+  
+#Question 4
+
+  function g(x)     #création de la nouvelle fonction
+    x=x.^2 .-2
+    return g(x)
+  end
