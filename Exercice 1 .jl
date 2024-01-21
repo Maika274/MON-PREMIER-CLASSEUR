4cc8801f403455e509220acfbb1c30a2a1b09179
Maika274 <153763945+Maika274@users.noreply.github.com>
2024-01-20 20:21:34 -0400

    Update Exercice (Dichotomie) .jl

--- Exercice 1 .jl
+++ Exercice 1 .jl
@@ -59,3 +59,13 @@ print("Pour $i, il faut $(n-1) itérations pour obtenir une erreur inférieure �
 end
 
   
+#Question 4
+
+  function g(x)     #création de la nouvelle fonction
+    x=x.^2 .-2
+    return g(x)
+  end
+
+  #Représentation graphique de l'évolution de l'erreur
+  plot(E,n-1, xaxis=(0:0,0001:3), yaxis=(1:1:n), title="Evolution de l'erreur en fonction du nombre d'itération", titlefont=font(14),lw=3,aspect_ratio=:equal)
+  
